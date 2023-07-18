d47a890628b96c8adf9cb991b610d737cb4f4990
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-18 18:27:03 +0100

    Update wrapper

--- analysis/try_extract.py
+++ analysis/try_extract.py
@@ -6,13 +6,14 @@
 # from simple_parsing import ArgumentParser
 
 from zipfile import ZipFile
-from typing import Dict, Any, Callable
+from typing import Dict, Any
 from pathlib import Path
 
 import pandas as pd
 from loguru import logger
 from bs4 import BeautifulSoup as bs
 from yiutils.failsafe import failsafe
+from yiutils.processing import processing_wrapper
 
 from funcs.paths import paths
 
@@ -20,6 +21,7 @@ INPUT_DIR = paths["raw_data_dir"] / "medrxiv" / "Current_Content" / "March_2023"
 assert INPUT_DIR.exists()
 OUTPUT_DIR = paths["tmp_output"]
 OUTPUT_FILE = OUTPUT_DIR / "tmp.csv"
+OUTPUT_ERROR_FILE = OUTPUT_DIR / "tmp_error.csv"
 
 NUM_SAMPLE = 30
 
@@ -38,25 +40,13 @@ def main_extract(input_zip: Path) -> Dict[str, Any]:
     return res
 
 
-def extract_wrapper(idx: int, total: int, payload: Dict[str, Any], func: Callable):
-    echo_step = 10
-    if idx % echo_step == 0:
-        logger.info(f"#{idx}/{total}, payload: {payload}")
-    fail_res = func(**payload)
-    if fail_res[1]:
-        res = fail_res[0]
-    else:
-        res = None
-    return res
-
-
 def main():
     all_file_list = [_ for _ in INPUT_DIR.iterdir() if str(_).endswith(".meca")]
     logger.info(f"Num files in {INPUT_DIR} {len(all_file_list)}")
     sample_file_list = all_file_list[:NUM_SAMPLE]
 
-    sample_res = [
-        extract_wrapper(
+    processing_res = [
+        processing_wrapper(
             idx=idx,
             total=len(sample_file_list),
             payload={"input_zip": _},
@@ -64,8 +54,18 @@ def main():
         )
         for idx, _ in enumerate(sample_file_list)
     ]
-    flatten_res = pd.DataFrame([_ for _ in sample_res if _ is not None])
+
+    flatten_res = pd.DataFrame([_[0] for _ in processing_res if _[0]])
     flatten_res.to_csv(OUTPUT_FILE, index=False)
+
+    error_res = pd.DataFrame(
+        [
+            {"error": _[1], "context": _[2]}
+            for _ in processing_res
+            if isinstance(_[1], Exception)
+        ]
+    )
+    error_res.to_csv(OUTPUT_ERROR_FILE, index=False)
 
 
 if __name__ == "__main__":

68ea2325cea8a4ba1e20b505564579cb1045ba90
YiLiu6240 <yi.liu6240@gmail.com>
2023-07-27 09:27:11 +0100

    Update analysis

--- analysis/funcs/parse.py
+++ analysis/funcs/parse.py
@@ -39,10 +39,12 @@ def main_extract(input_zip: Path) -> Dict[str, Any]:
     res = {
         "zip_path": zip_path,
         "full_text_file": full_text_file,
+        "doi": parse_res["doi"],
         "title": parse_res["title"],
         "version": parse_res["version"],
         "category": parse_res["category"],
         "year_month": parse_res["year_month"],
+        "publisher_id": parse_res["publisher_id"],
     }
     return res
 
@@ -50,6 +52,16 @@ def main_extract(input_zip: Path) -> Dict[str, Any]:
 def parse_full_text(
     full_text_dict: Dict[str, Any], full_text_bs: bs, verbose: bool = False
 ) -> Dict[str, Any]:
+    # doi
+    doi_find = (
+        py_.chain(full_text_dict)
+        .at(["article", "front", "article-meta", "article-id"])
+        .filter(lambda col: col["@pub-id-type"] == "doi")
+        .value()[0]
+    )
+    doi = doi_find["#text"]
+    assert isinstance(doi, str), {"doi": doi}
+
     # title
     title_find = (
         py_.chain(full_text_dict)
@@ -115,10 +127,25 @@ def parse_full_text(
         "month": int(accepted_date_find["month"]),
     }
 
+    # publisher_id, medrxiv or biorxiv
+    publisher_id_find = (
+        py_.chain(full_text_dict)
+        .at(["article", "front", "journal-meta", "journal-id"])
+        .value()[0]
+    )
+    publisher_id = publisher_id_find["#text"]
+    assert isinstance(publisher_id, str), {"publisher_id": publisher_id}
+
+    # publish info
+
     res = {
+        "doi": doi,
         "title": title,
         "version": version,
         "category": category,
         "year_month": year_month,
+        "publisher_id": publisher_id,
+        # NOTE: might revisit this later, after a formal round of extract
+        # "publish_info": publish_info,
     }
     return res
